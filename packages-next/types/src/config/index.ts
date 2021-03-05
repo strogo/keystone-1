gabidobo/keystone

Add config.graphql.apolloConfig

packages-next/types/src/config/index.ts
@@ -2,6 +2,7 @@ import type { ConnectOptions } from 'mongoose';
 import { CorsOptions } from 'cors';
 import type { GraphQLSchema } from 'graphql';
 import { IncomingMessage } from 'http';
+import type { ApolloConfig } from 'apollo-server-types';
 
 import type { ListHooks } from './hooks';
 import type { ListAccessControl, FieldAccessControl } from './access-control';
@@ -109,6 +110,11 @@ export type GraphQLConfig = {
   queryLimits?: {
     maxTotalResults?: number;
   };
+  /**
+   *  Additional options to pass into the ApolloServer constructor.
+   *  @see https://www.apollographql.com/docs/apollo-server/api/apollo-server/#constructor
+   */
+  apolloConfig?: ApolloConfig;
 };
 
 // config.extendGraphqlSchema

strogo/keystone-1

packages-next/types/src/config/index.ts
@@ -2,6 +2,7 @@ import type { ConnectOptions } from 'mongoose';
 import { CorsOptions } from 'cors';
 import type { GraphQLSchema } from 'graphql';
 import { IncomingMessage } from 'http';
+import type { Config } from 'apollo-server-express';
 
 import type { ListHooks } from './hooks';
 import type { ListAccessControl, FieldAccessControl } from './access-control';
@@ -109,6 +110,11 @@ export type GraphQLConfig = {
   queryLimits?: {
     maxTotalResults?: number;
   };
+  /**
+   *  Additional options to pass into the ApolloServer constructor.
+   *  @see https://www.apollographql.com/docs/apollo-server/api/apollo-server/#constructor
+   */
+  apolloConfig?: Config;
 };
 
 // config.extendGraphqlSchema